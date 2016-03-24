31b912e61f68657c1af270f56d8a2b4464002181
Erick2206 <aayushar@yahoo.com>
2016-03-24 13:25:13 +0530

    Added files via upload

---
+++ get_url.py
@@ -0,0 +1,43 @@
+from bing import bing_api
+import pprint
+
+# Stores name of the track
+#search_track=raw_input("Enter name of song: ")
+
+# Stores name of the artist
+#search_artist=raw_input("Enter name of artist: ")
+
+# Stores the final search query
+#search = search_artist + " " +  search_track + " azlyrics"
+
+def az_url(search):
+    '''
+    A function that returns the URL of azlyrics
+    '''
+    result=bing_api(search)
+
+
+    temp_url=[]
+
+    list_of_url=[]
+
+    for i in range(10):
+        list_of_url.append(result['d']['results'][i]['Url'])
+
+    for url in list_of_url:
+        temp_url.append(url.split('.'))
+
+    flag=0
+    counter=0
+
+    for url in temp_url:
+        counter+=1
+        if 'azlyrics' in url:
+            flag=1
+            break
+
+    if flag==0:
+        return "Sorry, No Data Found :-("
+
+    else:
+        return list_of_url[counter-1]
---
+++ lyrics.py
@@ -0,0 +1,29 @@
+from bing import bing_api
+from get_url import az_url
+from BeautifulSoup import *
+import urllib
+import pprint
+
+# Stores name of the track
+search_track=raw_input("Enter name of song: ")
+
+# Stores name of the artist
+search_artist=raw_input("Enter name of artist: ")
+
+search = search_artist + " " +  search_track + " azlyrics"
+
+url=az_url(search)
+
+if url!="Sorry, No Data Found :-(":
+    html=urllib.urlopen(url).read()
+    lyrics = BeautifulSoup(html)
+
+    # Retrieve all of the anchor tags
+    tags = lyrics.findAll('div')[10].findAll('div')
+
+    lyric=tags[11]
+    a=list(lyric)
+    for line in a:
+        if str(line)=='<br />' or str(line)=='<i>' or str(line)=='</i>':
+            continue
+        print line
